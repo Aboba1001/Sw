a2e7f40c3897b7add15f7374744eeb96d9505a8b
GioGitHub_ <112294807+Aboba1001@users.noreply.github.com>
2025-02-10 21:53:26 +0400

    update DeathMsg.lua

--- DeathMsg.lua
+++ DeathMsg.lua
@@ -27,8 +27,8 @@ local function TextAnimation()
   local dfSize = deathTxt.Size
   local zoomSize = UDim2.new(dfSize.Size.X.Scale * 1.1, 0, dfSize.Size.Y.Scale * 1.1, 0)
   
-  local zoomIn = TWS:Create(deathTxt, TweenInfo.new(0.3), {Size = UDim2.new(deathTxt.Size.X.Scale * 1.1, 0, deathTxt.Size.Y.Scale * 1.1, 0})
-  
+  local zoomTxt = TWS:Create(deathTxt, TweenInfo.new(0.3), Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {Size = zoomSize})
+  local zoomOut = TWS:Create(deathTxt, TweenInfo.new(1), Enum.)
   wait(4.5) -- After 4.5 seconds, the text disappears with anim
   TWS:Create(deathTxt, TweenInfo.new(1, Enum.EasingStyle.Quad, EnumEasingDirection.Out, 0, false, 0)
 end
