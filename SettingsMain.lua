cd8eed1b3eccc72c65d482cb7ce1d807f4e0fd96
GioGitHub_ <112294807+Aboba1001@users.noreply.github.com>
2025-02-11 01:18:16 +0400

    update SettingsMain.lua

--- SettingsMain.lua
+++ SettingsMain.lua
@@ -1,14 +1,14 @@
 local OptionButton = script.Parent
 local isOn = true
 
-script.Parent.MouseButton1click:Connect(function()
+OptionButton.MouseButton1click:Connect(function()
   if isOn == true then
     isOn = false
-    OptionButton.Text = "Less Lag: Off"
+    OptionButton.Text = "Less Lag: [Off]"
     game.Lighting.GlobalShadows = true
     else
       isOn = true
-      OptionButton.Text = "Less Lag: On"
+      OptionButton.Text = "Less Lag: [On]"
       game.Lighting.GlobalShadows = false
   end
 end)
