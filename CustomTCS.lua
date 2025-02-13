d1b125e871f8a5711c74d88b79194e80f168df1c
GioGitHub_ <112294807+Aboba1001@users.noreply.github.com>
2025-02-13 14:23:45 +0400

    update CustomTCS.lua

--- CustomTCS.lua
+++ CustomTCS.lua
@@ -1,12 +1,16 @@
-local TCS = game:GetServive("TextChatService")
+local TCS = game:GetService("TextChatService")
 local BubbleChat = TCS.BubbleChatConfiguration
-local ChatWindow = TCS.FindFirstChild("ChatWindow")
+local ChatWindow = TCS.ChatWindowConfiguration
+local ChatInputBar = TCS.ChatInputBarConfiguration
 
 local ChatBubbleBackgroundColor = Color3.fromRGB(255,255,255)
 local ChatBubbleTxtColor = Color3.fromRGB(0,0,0)
 
+local font = Font.new("rbxasset://fonts/families/FredokaOne.json")
 BubbleChat.TextColor3 = ChatBubbleTxtColor
 BubbleChat.BackgroundColor3 = ChatBubbleBackgroundColor
-BubbleChat.FontFace(Font.new("rbxasset://fonts/families/FredokaOne.json"))
+BubbleChat.FontFace = font
 
-ChatWindow.FontFace(Font.new("rbxasset://fonts/families/FredokaOne.json"))
+ChatWindow.FontFace = font
+
+ChatInputBar.FontFace = font
