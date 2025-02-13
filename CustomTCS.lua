70196cbaf355a8ffffb82193acdf898f2163d0b3
GioGitHub_ <112294807+Aboba1001@users.noreply.github.com>
2025-02-13 14:26:27 +0400

    update CustomTCS.lua

--- CustomTCS.lua
+++ CustomTCS.lua
@@ -9,8 +9,7 @@ local ChatBubbleTxtColor = Color3.fromRGB(0,0,0)
 local font = Font.new("rbxasset://fonts/families/FredokaOne.json")
 BubbleChat.TextColor3 = ChatBubbleTxtColor
 BubbleChat.BackgroundColor3 = ChatBubbleBackgroundColor
+
 BubbleChat.FontFace = font
-
 ChatWindow.FontFace = font
-
 ChatInputBar.FontFace = font
