8d38f8506398de947d2e18e980d548cd05007907
GioGitHub_ <112294807+Aboba1001@users.noreply.github.com>
2025-03-09 21:26:57 +0400

    Update Option.lua

--- Option.lua
+++ Option.lua
@@ -1,36 +1,61 @@
 -- Day/night
 local Lighting = game:GetService("Lighting")
 
-local isDay = false
 local TextLabel = script.Parent
+local ClickSFX = workspace.SFX.ClickSFX
 
-TextLabel.Text = "Time: Day"
+local isNight = false
+
+TextLabel.Text = "Time : Day"
 
 TextLabel.MouseButton1Click:Connect(function()
-  if not isDay then
-    TextLabel.Text = "Time: Day"
+  ClickSFX:Play()
+  if not isNight then
+    TextLabel.Text = "Time : Day"
     Lighting.ClockTime = 14
-    isDay = true
+    isNight = true
     else
-      TextLabel.Text = "Time: Night"
+      TextLabel.Text = "Time : Night"
       Lighting.ClockTime = 0
-      isDay = false
+      isNight = false
   end
 end)
   
 -- Shadows
 local Lighting = game:GetService("Lighting")
 
-local isShadows = false
 local TextLabel = script.Parent
+local isShadows = false
+
+TextLabel.Text = "Shadows : ON"
 
 TextLabel.MouseButton1Click:Connect(function()
+  ClickSFX:Play()
   if not isShadows then
-    TextLabel.Text = "Shadows: ON"
+    TextLabel.Text = "Shadows : ON"
     Lighting.GlobalShadows = true
     else
-      TextLabel.Text = "Shadows: OFF"
+      TextLabel.Text = "Shadows : OFF"
       Lighting.GlobalShadows = false
       isShadows = false
+  end
+end)
+
+-- Vibrations
+local HS = GetService("HapticService")
+local UIS = GetService("UserInputService")
+
+local TextLabel = script.Parent
+local isVibrated = true
+
+TextLabel.Text = "Vibration : ON"
+TextLabel.MouseButton1Click:Connect(function()
+  ClickSFX:Play()
+  if isVibrated then
+    TextLabel.Text = "Vibration: OFF"
+    isVibrated = false
+  else
+    TextLabel.Text = "Vibration: ON"
+    isVibrated = true
   end
 end)
