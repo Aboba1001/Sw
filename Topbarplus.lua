8881aa2c33c7a0d49870efc8445a908e4f5f26d6
GioGitHub_ <112294807+Aboba1001@users.noreply.github.com>
2025-03-12 02:31:31 +0400

    update Topbarplus.lua

--- Topbarplus.lua
+++ Topbarplus.lua
@@ -5,8 +5,8 @@ local Topbar = require(game.ReplicatedStorage.Topbar.Icon)
 
 local Music = workspace.Music
 
-local MainGui = Player:WaitForChild("PlayerGui"):WaitForChild("Main")
-local SettingsMenu = MainGui:WaitForChild("Settings")
+local MainGui = Player:WaitForChild("StarterGui"):WaitForChild("Main")
+local SettingsMenu = MainGui:WaitForChild("Settings") 
 local CommandsMenu = MainGui:WaitForChild("Commands")
 local AchievementsMenu = MainGui:WaitForChild("Achievements")
 
