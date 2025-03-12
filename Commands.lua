5eb1375d01e57b8a07255ab377142ad20b49e022
GioGitHub_ <112294807+Aboba1001@users.noreply.github.com>
2025-03-12 05:27:55 +0400

    update Commands.lua

---
+++ Commands.lua
@@ -0,0 +1,10 @@
+local TeleportService = GetService("TeleportService")
+local Lighting = GetService("Lighting")
+
+
+local prefix = "@"
+
+local cmds = {
+  [prefix.."rejoin"] = function()
+  end,
+}
